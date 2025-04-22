736659a74d6b17d878ff5c07369a5860a142fb7e
taewoongheo <chamjoeun0111@gmail.com>
2025-04-22 20:25:20 +0900

    feat: add expandable option panel on goal text tap

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,25 +1,66 @@
 import {Layout} from '@/constants/Layout';
 import {Typography} from '@/constants/Typography';
+import {useState} from 'react';
 import {StyleSheet, View, Text} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+
+const goal = '(목표)';
+const dDay = '(디데이)';
+const achieved = '(달성한 일들)';
+const remaining = '(할 일들)';
 
 export default function MainScreen() {
-  const goal = '(목표)';
-  const dDay = '(디데이)';
-  const achieved = '(달성한 일들)';
-  const remaining = '(할 일들)';
+  const [goalExtend, setGoalExtend] = useState<boolean>(false);
+
+  const handleGoalPress = () => {
+    console.log('tap goal');
+    setGoalExtend(prev => !prev);
+  };
+
+  const handleDDayPress = () => {
+    console.log('tap dDay');
+  };
+
+  const handleAchievedPress = () => {
+    console.log('tap achieved');
+  };
+
+  const handleRemainingPress = () => {
+    console.log('tap Todo');
+  };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>
-        <Text style={styles.highlight}>{goal}</Text>까지{' '}
-        <Text style={styles.highlight}>{dDay}</Text> 남았어요.
-      </Text>
-      <Text style={styles.text}>
-        지금까지 <Text style={styles.highlight}>{achieved}</Text>을 해냈고,
-      </Text>
-      <Text style={styles.text}>
-        앞으로 <Text style={styles.highlight}>{remaining}</Text>이 남았어요.
-      </Text>
+      <View style={styles.textContainer}>
+        <TouchableOpacity onPress={handleGoalPress}>
+          <View
+            style={{
+              flexDirection: 'row',
+            }}>
+            <Text style={[styles.text, styles.highlight]}>{goal}</Text>
+            {goalExtend && (
+              <Text style={[styles.text, styles.highlight]}>설정</Text>
+            )}
+          </View>
+        </TouchableOpacity>
+        <Text style={styles.text}> 까지 </Text>
+        <TouchableOpacity onPress={handleDDayPress}>
+          <Text style={[styles.text, styles.highlight]}>{dDay}</Text>
+        </TouchableOpacity>
+        <Text style={styles.text}> 남았어요.</Text>
+
+        <Text style={styles.text}>지금까지 </Text>
+        <TouchableOpacity onPress={handleAchievedPress}>
+          <Text style={[styles.text, styles.highlight]}>{achieved}</Text>
+        </TouchableOpacity>
+        <Text style={styles.text}>을 해냈고,</Text>
+
+        <Text style={styles.text}>앞으로 </Text>
+        <TouchableOpacity onPress={handleRemainingPress}>
+          <Text style={[styles.text, styles.highlight]}>{remaining}</Text>
+        </TouchableOpacity>
+        <Text style={styles.text}>이 남았어요.</Text>
+      </View>
     </View>
   );
 }
@@ -29,6 +70,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     paddingHorizontal: Layout.padding.horizontal,
+  },
+  textContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'center',
   },
   text: {
     fontSize: Typography.fontSize.large,

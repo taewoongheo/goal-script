5465e4e7d28b9fcf46e62bd419b99e4a32cffcbe
Taewoong Heo <chamjoeun0111@gmail.com>
2025-06-14 19:57:02 +0900

    chore: update README

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-# goal-script
+# textual
 
-text-based goal tracker app
+<img src="https://github.com/user-attachments/assets/1bd44e68-8d52-4591-a069-8cdfd1e3b282" width="300" height="auto" />
+
+<img src="https://github.com/user-attachments/assets/261098b9-4a30-4b61-b5e0-b5b7ae61e4f4" width="300" height="auto" />
+
+<img src="https://github.com/user-attachments/assets/933b586f-81ca-49ea-84e2-ef1ce752ce93" width="300" height="auto" />
+
+<img src="https://github.com/user-attachments/assets/fd04f101-123d-4cbc-a176-dfbd0ac96218" width="300" height="auto" />
+
+<img src="https://github.com/user-attachments/assets/e9372068-ee02-4d9a-9b21-ee9925cde6a2" width="300" height="auto" />
+
+<img src="https://github.com/user-attachments/assets/557a857c-41f7-4516-92cf-10b6fa105533" width="300" height="auto" />
--- app/privacy-policy.tsx
+++ app/privacy-policy.tsx
@@ -9,9 +9,9 @@ import {
 } from 'react-native';
 import {router} from 'expo-router';
 import {AntDesign} from '@expo/vector-icons';
+import {scale} from 'react-native-size-matters';
 import {Theme} from '@/constants/Theme';
 import {Colors} from '@/constants/Colors';
-import {scale} from 'react-native-size-matters';
 
 export default function PrivacyPolicyScreen() {
   return (
@@ -36,9 +36,9 @@ export default function PrivacyPolicyScreen() {
         showsVerticalScrollIndicator={false}>
         <View style={styles.contentContainer}>
           <Text style={styles.introText}>
-            Textual(이하 "회사")는 개인정보보호법에 따라 이용자의 개인정보를
-            보호하고 이와 관련한 고충을 신속하고 원활하게 처리할 수 있도록 하기
-            위하여 다음과 같이 개인정보처리방침을 수립·공개합니다.
+            Textual은 개인정보보호법에 따라 이용자의 개인정보를 보호하고 이와
+            관련한 고충을 신속하고 원활하게 처리할 수 있도록 하기 위하여 다음과
+            같이 개인정보처리방침을 수립·공개합니다.
           </Text>
 
           <Text style={styles.sectionTitle}>제1조. 개인정보의 처리 목적</Text>
